629f459af2eb2125816c1374c4a22eedfed7c65c
Emad Saber Hamed <omda.sharkawy@live.com>
2024-08-31 01:10:40 +0300

    Lab 7 Completed

--- 07/After/ABPCourse.Demo1/aspnet-core/src/ABPCourse.Demo1.Application/Demo1ApplicationModule.cs
+++ 07/After/ABPCourse.Demo1/aspnet-core/src/ABPCourse.Demo1.Application/Demo1ApplicationModule.cs
@@ -1,5 +1,9 @@
-﻿using Volo.Abp.Account;
+﻿using Microsoft.Extensions.Caching.StackExchangeRedis;
+using System;
+using Volo.Abp.Account;
 using Volo.Abp.AutoMapper;
+using Volo.Abp.Caching;
+using Volo.Abp.Caching.StackExchangeRedis;
 using Volo.Abp.FeatureManagement;
 using Volo.Abp.FluentValidation;
 using Volo.Abp.Identity;
@@ -19,6 +23,7 @@ namespace ABPCourse.Demo1;
     typeof(AbpTenantManagementApplicationModule),
     typeof(AbpFeatureManagementApplicationModule),
     typeof(AbpSettingManagementApplicationModule),
+    typeof(AbpCachingStackExchangeRedisModule),
     typeof(AbpFluentValidationModule)
     )]
 public class Demo1ApplicationModule : AbpModule
@@ -28,6 +33,12 @@ public class Demo1ApplicationModule : AbpModule
         Configure<AbpAutoMapperOptions>(options =>
         {
             options.AddMaps<Demo1ApplicationModule>();
+        });
+
+        Configure<AbpDistributedCacheOptions>(options =>
+        {
+            options.KeyPrefix = "Demo1_";
+            options.GlobalCacheEntryOptions.AbsoluteExpiration = DateTimeOffset.Now.AddHours(1);
         });
     }
 }
